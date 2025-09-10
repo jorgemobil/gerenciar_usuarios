8696ade38dc6473e416185d930cce73a8d20cb56
Bowie <marketing@letscode.com.br>
2025-09-09 22:58:39 -0300

    adicionando nova função do javascript e criação de lista de objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,4 +1,23 @@
-const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+//const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+
+const usuarios = [
+//objeto            
+    {      //propriedade / valor da propriedade
+            nome:'Jorge',
+            email:'jorgemobil@live.com'
+    },
+
+    {
+            nome:'Jaqueline',
+            email:'jaqueforrico@gmail.com'
+    },
+
+    {
+            nome:'Gustavo',
+            email:'gorrico797@hotmail.com'
+    }   
+
+];
 
 function retornarNomeUsuarios() {
     return usuarios;
@@ -6,8 +25,8 @@ function retornarNomeUsuarios() {
 
 // a função abaixo adiciona um novo nome a lista de usuarios
 // push usado para incluir o "nome" na lista de usuarios.
-function adicionarNovoUsuario(nome) {
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario) {
+    usuarios.push(usuario);
 }
 
 module.exports = {
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,13 +4,22 @@ const { expect } = require('chai');
 describe('Testar as funções de Gestão de Usuarios', function () {
     it('Validar que posso adicionar um novo  nome de usuario na lista', function () {
         // 1. Adicionar um novo nome na lista de usuarios
-        adicionarNovoUsuario('Maria');
+        adicionarNovoUsuario({
+            nome: 'Maria',
+            email: 'maria@gospel.com'
+            
+        });
 
         // 2. Retornar a lista de usuarios na caixa lista de usuários
         const listaDeUsuarios = retornarNomeUsuarios();
 
-        // 3. Comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+        // 3. Comparar se o novo nome está no fim da lista de usuários e espero que tenha o "nome" igual Maria
+        expect(listaDeUsuarios.at(-1)).to.deep.equal
+        ({
+            nome: 'Maria',
+            email: 'maria@gospel.com'
+        });
+        
 
 });
 
